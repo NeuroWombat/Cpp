be4838ac4c66223d91a993f3711a0f6bfe5180cd
WombasticDeveloper <kkamil.kula@gmail.com>
2024-11-08 21:20:08 +0100

    Add multiple disk support and improve display formatting

--- Zad4.cpp
+++ Zad4.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include<string>
 using namespace std;
 
 struct Krazek{
@@ -20,20 +21,23 @@ struct Stos{
         curr=nullptr;
     }
 
-    void addK(int dl){      // Dodawanie krążka
-        curr=new Krazek(dl);
-        curr->next=head;
-        head=curr;
+    void addK(int dl,int n){      // Dodawanie krążka
+        for(int i=0;i<n;i++){
+            curr=new Krazek(dl-2*i);
+            curr->next=head;
+            head=curr;
+        }
     }
 
-    void display(){         // Wyświetlanie krążków
+    void display(int nrStosu, int n){         // Wyświetlanie krążków
         Krazek* temp = head;
-        int a=0;
+
+        cout<<"Stos nr"<<nrStosu<<endl;
         while (temp != nullptr){
-            a++;
+            cout<<string((n-temp->dl)/2,' ')<<string(temp->dl, '-')<<string((n-temp->dl)/2+1,' ')<<"("<<temp->dl<<")"<<endl;
             temp = temp->next;  // Przechodzimy do następnego krążka
         }
-        cout<<"Stos nr"<< " | Ilosc elementow: "<<a<<endl;
+        cout<<string((n-5)/2,' ')<<"__|__\n";
     }
 };
 
@@ -50,7 +54,6 @@ bool wygrana(Stos& S, int n){
         temp=temp->next;
     }
 
-    cout<<ilosc;
     if (ilosc==n){
         return true;
     }
@@ -60,26 +63,23 @@ bool wygrana(Stos& S, int n){
 
 
 int main(){
-    Stos S1, S2, S3;
-    Stos* stosy[3]={&S1, &S2, &S3};
-    int zStosu, doStosu, n;
+    Stos Stosy[3];
+    int zS, doS, n;
     cout<<"Podaj ilosc krazkow [3-10]: ";cin>>n;
+    int dl=n+3;
+    Stosy[0].addK(dl,n);
 
-    for(int i=1;i<=n;i++){ // Wypełnianie pierwszego stosu krążkami
-        S1.addK(n-i);
-    }
+    while(!wygrana(Stosy[2],n)){
+        Stosy[0].display(1,dl);
+        Stosy[1].display(2,dl);
+        Stosy[2].display(3,dl);
 
-    while(!wygrana(S3,n)){
-        S1.display();
-        S2.display();
-        S3.display();
-
-        cout<<"Z jakiego stosu chcesz przelozyc krazek? ";cin>>zStosu;
-        cout<<"Na jaki stos chcesz polozyc krazek X.k ze stosu X? ";cin>>doStosu;
-        if(zStosu>=1 && zStosu<=3 && doStosu>=1 && zStosu<=3 && zStosu!=doStosu){
-            przenoszenie(*stosy[zStosu-1], *stosy[doStosu-1]);
+        cout<<"\n\nZ jakiego stosu chcesz przelozyc krazek? ";cin>>zS;
+        cout<<"Na jaki stos chcesz polozyc krazek X.k ze stosu X? ";cin>>doS;
+        if(zS>=1 && zS<=3 && doS>=1 && zS<=3 && zS!=doS){
+            przenoszenie(Stosy[zS-1], Stosy[doS-1]);
         }else{
-            cout<<"";
+            cout<<"Przenies ";
         }
     }
 
